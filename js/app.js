6395a5b82e3eed9d94e593120e586fd604cdc6b4
Hiddensti <alrahmanihadi@gmail.com>
2025-01-20 17:11:01 +0300

    snake moving part 1

--- js/app.js
+++ js/app.js
@@ -28,11 +28,56 @@ let createDiv = ()=> {
 }
 createDiv()
 
+const cell183El = document.querySelector('.cell-183');
+// const cell184El = document.querySelector('.cell-184');
+
+// creating the snake on the board
+const createSnake = document.createElement("div")
+// give the snake a (class="theSnake")
+createSnake.classList.add('theSnake')
+// put the snake inside the cell-183 div
+cell183El.append(createSnake)
 
 
+// console.log(createSnake);
+
+// let snakePosition = 183;
+// let snakeMovements = () => {
+//     for (i=0; i < 16; i++) {
+//         const cell = document.querySelector(`.cell-${i}`)
+//         cell.remove(createSnake)
+//         createSnake.append(div)
+//     } 
+// }
 
 
-//! <!-- CODE GRAVEYARD --> 
+let celNum = 183
+document.addEventListener('keydown', (e) => {
+    //    console.log('key is pressed.')
+       if (e.key === "ArrowUp") {
+        console.log("up arrow pressed");
+    } else if (e.key === "ArrowDown") {
+        console.log("down arrow pressed");
+    } else if (e.key === "ArrowLeft") {
+        console.log("left arrow pressed");
+    } else if (e.key === "ArrowRight") {
+        for (i=0; i < 100; i++){
+
+            // setInterval(()=> {            
+            //     console.log(celNum)
+            //     createSnake.remove(document.querySelector(`cell-${celNum}`))
+            //     celNum = celNum + 1 
+            //     // or celNum++
+                
+            //     document.querySelector(`.cell-${celNum}`).append(createSnake);
+            // } 
+            //     , 1000)
+            
+            } 
+        }
+       })
+    
+// ! <!-- CODE GRAVEYARD --> 
 
 // console.log('Its working!');
 
@@ -43,3 +88,16 @@ createDiv()
 // console.log('Button was clicked!')
 // location.href = "./game.html"
 // });
+
+////////////////////////////////
+// //selecting (thesnake) div - using 'let' cuz snake gonna move around using arrow keys.
+// let snake = document.querySelector('theSnake');
+// // how many px the snake head will move each time arrow key is pressed.
+// // const modifier = 25;
+// //loading the snake in that position
+// window.addEventListener('load', () => {
+//     snake.style.position = 'abolute';
+//     snake.style.left = 0;
+//     snake.style.top = 0;
+// })
+
